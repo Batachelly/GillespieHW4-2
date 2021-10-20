7e8c58377a231f2eadcb9d061665c8c60522d527
Demo User <debian@beaglebone.localdomain>
2021-10-20 19:41:20 +0000

    Homework for cpe 422

--- subtractMachine.sh
+++ subtractMachine.sh
@@ -3,10 +3,15 @@
 #This Bash script takes 2 command line arguments and
 #Subtracts the smaller from the larger and then prints out the difference to stdout
 #Then Counts down from the difference to 1 while printing each count to stdout
+#Example usage: ./subtractMachine 3 8
+
+echo
+echo "Starting the subtractMachine Bash Script"
 
 ARG1=$1
 ARG2=$2
 
+#function
 subtract() {
 
 if [ $ARG1 -gt $ARG2 ]
@@ -20,7 +25,15 @@ fi
 }
 
 
-# Can also use the variable names instead
+# if not used corectly
+
+if [ $# -eq 0 ]; then
+	echo "There are no arguments. Usage is:"
+	echo -e " subtractMachine arg1 arg2 \n where arg1 and arg2 are integers "
+	echo -e "Usage: \n subtractMachine 3 5 "
+    exit 2
+fi
+
 
 
 echo
